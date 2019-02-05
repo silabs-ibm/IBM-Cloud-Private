674e408eaa39b38ef1d8e7d403b61f6cf46e89bb
abalasu1 <abalasu1@in.ibm.com>
2019-02-05 21:17:41 +0530

    add docker buidl step to maven

--- Common/Calculator-Spring-Boot/calculator-processor/src/main/java/com/calculator/processor/ProcessorApplication.java
+++ Common/Calculator-Spring-Boot/calculator-processor/src/main/java/com/calculator/processor/ProcessorApplication.java
@@ -3,6 +3,7 @@ package com.calculator.processor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -18,5 +19,6 @@ public class ProcessorApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
-	}	
+	}
+
 }
--- Common/Calculator-Spring-Boot/composite-op/src/main/java/com/calculator/compositeop/controller/CompositeOpController.java
+++ Common/Calculator-Spring-Boot/composite-op/src/main/java/com/calculator/compositeop/controller/CompositeOpController.java
@@ -5,7 +5,6 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
