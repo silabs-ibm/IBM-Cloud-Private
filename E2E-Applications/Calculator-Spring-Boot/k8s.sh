1a0d1f2c857dc486547bf6c522f7f813f2cf2a86
abalasu1 <abalasu1@in.ibm.com>
2019-02-16 17:16:56 +0530

    updates to calcspringboot

--- E2E-Applications/Calculator-Spring-Boot/k8s.sh
+++ E2E-Applications/Calculator-Spring-Boot/k8s.sh
@@ -13,14 +13,14 @@ kubectl delete -f k8s/basic-op-multiply.yaml
 kubectl delete -f k8s/basic-op-subtract.yaml
 kubectl delete -f k8s/basic-op-add.yaml
 
-kubectl delete -f k8s/calculator-discovery.yaml
+kubectl delete -f k8s/discovery.yaml
 kubectl delete -f k8s/zipkin.yaml
 
 # Create new deployments
 kubectl apply -f imagepolicy.yaml
 kubectl apply -f ingress.yaml
 
-kubectl apply -f k8s/calculator-discovery.yaml
+kubectl apply -f k8s/discovery.yaml
 kubectl apply -f k8s/zipkin.yaml
 
 kubectl apply -f k8s/processor.yaml
